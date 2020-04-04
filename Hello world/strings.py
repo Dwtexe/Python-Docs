f498ebeb625d368cb7a7a8a0249c1b65f113bf48
Dwtexe <63106683+Dwtexe@users.noreply.github.com>
2020-04-04 23:49:15 +0300

    strings.py
    strings summary

---
+++ Hello world/strings.py
@@ -0,0 +1,16 @@
+name = "Selami"
+surname = "Çakmaktaş"
+age = 1046
+
+greeting = "My name is " , name , " " , surname , " and \nI am ", age , 'years old.'
+lenght = len(greeting)
+
+#print(greeting)
+#print(greeting[0])
+#print(greeting[3])
+#print(greeting[lenght-1])
+#print(greeting[-1])
+#print(greeting[3:7])
+#print(greeting[3:])
+#print(greeting[:16])
+#print(greeting[2:40:3])
