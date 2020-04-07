c47c7f410eb9182d60168bc195176eeeab1da776
Dwtexe <63106683+Dwtexe@users.noreply.github.com>
2020-04-07 21:12:23 +0300

    string-methods summary

---
+++ Hello world/string-methods.py
@@ -0,0 +1,24 @@
+message = ' Hello there. My name is Dwt Exe'
+
+# message = message.upper()      # Bu metod ile message değişkeninin içinde bulunan bütün kararkterleri büyük harfe çevirir.
+# message = message.lower()      # Bu metod uppper metodunun tam tersidir tüm karakterleri küçük harfli yapar.
+# message = message.title()      # Bu metod ile değişkendeki tüm kelimelerin baş harfi büyük olarak değişir.
+# message = message.capitalize() # Bu metod değişkenin içindeki karakterlerin ilkini büyük harfe çevirir.
+# message = message.strip()      # Bu metod ile değikenin içindeki ilk karakter boşluk ise o boşluğu silip bize çıktı olarak verir.
+# message = message.split()      # Bu metod ile parantezin içine yazılan şeyden itibaren değişkenin içindeki karakterleri ayırarak diziler oluşturlur. İçinde bişey yoksa boşluk varsayar. (dizileri ileride öğrenicez bende bilmiyorum daha :d)
+# message = ''.join(message)     # Bu metod dizi oluşturulduğunda dizileri tekrar eski haline getirirken aralara karakter eklemenizi sağlar.
+# index = message.find('Dwt')    # Bu metod parantez içinde yazan karakterleri değişken içinde arar ve başlangıç karakterini çıktı olarak verir.
+# print(index)
+# message = message.replace('Dwt','hello')  # ctrl+f yaptığımız da çıkan bul ve değiştirin aynısıdır dwt yi bulup hello ile değiştirir.
+# message = message.center()   # Bu metod parantezin içine yazıldığı sayı kadar değişkene boşluk ekler değişkendeki karakterler de dahil olur ve mesajı tam ortalar.
+
+'''
+diğer metodları bulabilirsiniz:
+
+https://www.w3schools.com/python/python_strings.asp
+https://yazilimhanem.com/python-strings-metodlar/
+http://justpythonhacker.blogspot.com/p/string-metodlar.html
+https://www.mobilhanem.com/python-karakter-dizileri/
+https://docs.python.org/3/library/string.html
+'''
+print(message)
