5a10073384904ee98b200cf7906cf8a9387a5374
Dwtexe <63106683+Dwtexe@users.noreply.github.com>
2020-06-26 23:06:28 +0300

    ufak düzenlemeler ve yeni bir proje klasörü

---
+++ Hello world/projelerim/kendini_tanıt.py
@@ -0,0 +1,8 @@
+#sizi birden fazla dilde tanıtan araç.
+
+name = input("Adınız nedir? ")
+surname = input("Soyadınız nedir? ") 
+age = input("Yaşınız kaç? ")
+
+greeting = "My name is "+ name + " "+ surname + " and \nI'm "+ str(age) + " years old."
+print(greeting)
