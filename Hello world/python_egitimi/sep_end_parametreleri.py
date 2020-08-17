7ae5bb8fcd32df6d304d581f1e38eab4c4335b72
Dwtexe <mamiolumcerrahi@gmail.com>
2020-08-17 14:36:52 +0300

    unutulmuş ders

---
+++ Hello world/python_egitimi/sep_end_parametreleri.py
@@ -0,0 +1,17 @@
+print("a","b","c","d",sep="--")
+#çıktı
+#a--b--c--d
+"""
+sep olmadığı zaman tırnakların rasındaki ',' ler boşluk olarak gözükür
+sep ile virgüllerin yerine istediğimizi koyabiliriz
+"""
+
+
+"""
+end parametresinde çıktının sonunda yer alacak ifade belirlenir
+"""
+#örnekler
+
+print("Selimiye Camii",end=" end kullandık")
+print("Selimiye Camii",end="")
+print("Selimiye Camii",end="\n")
