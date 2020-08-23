b8dae67abd23d0156f98b66a2d4a70f5cc463e22
Dwtexe <63106683+Dwtexe@users.noreply.github.com>
2020-08-23 23:12:02 +0300

    ....

--- Hello world/Sql/kütüphane_sql.py
+++ Hello world/Sql/kütüphane_sql.py
@@ -1,8 +1,18 @@
 import sqlite3
 
+# deepcode ignore missing~close~sqlite3.connect: <çünkü...>
 baglanti = sqlite3.connect("database.db")
 
 if baglanti:
     print("Başarılı...")
 else:
     print("Başarısız...")
+
+pen = baglanti.cursor()
+
+pen.execute("""
+    CREATE TABLE kitaplar(
+    kitap_no INTEGER PRIMARY KEY,
+    kitap_adi VARCHAR(50),
+    kitap_kategori INTEGER(2) )
+""")
